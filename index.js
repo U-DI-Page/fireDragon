8d99bf4e6494d61a4e6d2b6f361225883e1583a9
yerg <yeronggen@xiaoyouzi.com>
2019-10-29 13:49:34 +0800

    ss

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const iconv = require('iconv-lite')
 const fs = require('fs')
 
 const fileData = fs.readFileSync('./cubizhiyu.txt', 'utf8')
-                  .replace(/\r/g, '')
-                  .split('\n')
-                  .filter(i => !!i)
+                   .replace(/\r/g, '')
+                   .split('\n')
+                   .filter(i => !!i)
 const cache = []
 const single = {
   1: '.',
@@ -31,7 +31,7 @@ new Promise(resolve => {
     let randomIndex = makeRandomIndex()
     const text = fileData[randomIndex]
     exec('clip').stdin.end(iconv.encode(text, 'gbk'))
-    console.log(new Date(), text)
+    console.log(new Date().Format("yyyy-MM-dd mm:ss"), text)
   }, 500)
 })
 
@@ -45,4 +45,22 @@ function makeRandomIndex(){
 
   cache.push(randomIndex)
   return randomIndex
+}
+
+Date.prototype.Format = function (fmt) {
+  var o = {
+      "M+": this.getMonth() + 1, //月份 
+      "d+": this.getDate(), //日 
+      "h+": this.getHours(), //小时 
+      "m+": this.getMinutes(), //分 
+      "s+": this.getSeconds(), //秒 
+      "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+      "S": this.getMilliseconds() //毫秒 
+  }
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length))
+  }
+  for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)))
+  return fmt
 }
